f47f4d1dc9eb61753fd14b4f4935f42afcdbc0e5
Youngeun Suh <westzerosilver@gmail.com>
2023-10-12 21:19:44 +0900

    231012/백준 1단계 입출력과 사칙연산, 2단계 조건문

---
+++ src/No10171.java
@@ -0,0 +1,10 @@
+//https://acmicpc.net/problem/10171
+
+public class No10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
---
+++ src/No11382.java
@@ -0,0 +1,22 @@
+//https://www.acmicpc.net/problem/11382
+//런타임 에러 - 10의 12 제곱까지 입력범위인데, nextInt 로 입력받았음. nextLong 으로 입력해야 함.
+
+import java.util.Scanner;
+
+public class No11382 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Long[] num = new Long[3];
+        Long sum = 0L;
+        for (int i=0; i<3; i++) {
+            num[i] = s.nextLong();
+            if (num[i] < 1 || num[i] > Math.pow(10,12)) {
+                System.out.println("숫자를 다시 입력하세요");
+                num[i] = s.nextLong();
+            }
+            sum += num[i];
+        }
+        System.out.println(sum);
+
+    }
+}
