d39b749b873f9308fa92588f5625c228f9c610ca
Youngeun Suh <westzerosilver@gmail.com>
2023-10-13 21:09:22 +0900

    231013/백준 3단계 반복문

---
+++ src/No25314.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class No25314 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        for (int i=0; i<num/4; i++) {
+            System.out.print("long ");
+        }
+        System.out.println("int");
+    }
+}
