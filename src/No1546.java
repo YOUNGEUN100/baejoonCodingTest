3cbb7b85c4175b679b493bf18ed8af62e72709b0
Youngeun Suh <westzerosilver@gmail.com>
2023-10-15 22:39:41 +0900

    231013/백준 4단계 1차원 배열(2)

---
+++ src/No1546.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class No1546 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int cnt = s.nextInt();
+        double[] test = new double[cnt];
+        double max = 0;
+        for (int i=0; i<cnt; i++) {
+            test[i] = s.nextInt();
+            if (max < test[i]) {
+                max  = test[i];
+            }
+        }
+        double sum = 0;
+        for (int i=0; i<cnt; i++) {
+            test[i] = test[i] / max * 100;
+            sum += test[i];
+        }
+        System.out.println(sum/cnt);
+    }
+}
